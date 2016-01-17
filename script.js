9ef590e62f30aac8d137f9e8bf66e6463219954c
Tony Li <tony.li@seeq.com>
2016-01-17 14:24:11 -0500

    Added basic design

---
+++ script.js
@@ -0,0 +1,27 @@
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+
+var numberOfParticles = .00005 * (windowWidth * windowHeight);
+
+function addParticle() {
+    if (window.pJS_GUI.particles.array.length > numberOfParticles) {
+        clearTimeout(timeOut);
+        return;
+    }
+
+    window.pJS_GUI.particles.array.push(new window.pJS_GUI.fn.particle(
+        {value: '#fff'},
+        100
+    ));
+
+    timeOut = setTimeout(addParticle, 60);
+}
+
+function afterParticlesLoaded() {
+    console.log('callback - particles.js config loaded');
+    window.pJS_GUI = window.pJSDom[0].pJS;
+
+    addParticle();
+}
+
+particlesJS.load('particles-js', 'particle_config.json', afterParticlesLoaded);
